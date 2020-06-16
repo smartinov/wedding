fd055291d5dd8643f598ce875482befd2286639b
Stefan Martinov <stefan.martinov@gmail.com>
2020-06-16 18:21:38 +0200

    update publish script

--- publish.sh
+++ publish.sh
@@ -13,7 +13,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -22,4 +22,4 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "publish to gh-pages (publish.sh)"
